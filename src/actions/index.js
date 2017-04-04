52a3c2d6f20b756f1c65bb04e1fd3df9bacf35bc
Susan <wangjm323@gmail.com>
2017-04-03 22:19:49 -0400

    Install axios and add payload to action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
+
 const API_KEY = '4ee850a0d743d29ce63e07d3386a9fc6';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    // request is a promise returned by axios get method
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
